669336aa466423bcb13acbd08598d0ae1b55079b
Dustin Alandzes <dustin.alandzes@hp.com>
2025-02-27 11:49:52 -0600

    Plane class and tests

--- classes/Plane.js
+++ classes/Plane.js
@@ -1,6 +1,19 @@
 class Plane {
+    #passengers;
+    constructor(company, origin, destination) {
+        this.company = company;
+        this.origin = origin;
+        this.destination = destination;
+        this.#passengers = [];
+    }
 
+    getPassengers() {
+        return this.#passengers
+    }
 
+    setPassengers(person) {
+        this.#passengers.push(person)
+    }
 }
 
 
