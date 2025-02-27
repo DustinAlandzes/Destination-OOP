76483e4a64bbf48fce3a96fdafb6a92f7cdceb0e
Dustin Alandzes <dustin.alandzes@hp.com>
2025-02-27 11:38:07 -0600

    Bag

--- classes/Bag.js
+++ classes/Bag.js
@@ -1,6 +1,18 @@
 class Bag {
+    #owner
+    constructor(weight, id) {
+        this.weight = weight;
+        this.id = id;
+        this.#owner = null;
+    }
 
+    getOwner() {
+        return this.#owner;
+    }
 
+    setOwner(owner) {
+        this.#owner = owner;
+    }
 }
 
 
